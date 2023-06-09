75b492cdbcde1bdfded5a822853116b64dcede66
Allan <allandrs@gmail.com>
2023-06-08 22:52:27 -0300

    Continuacao exercicios

--- src/TestaMetodo.java
+++ src/TestaMetodo.java
@@ -1,12 +1,34 @@
 public class TestaMetodo {
     public static void main(String[] args) {
-        Conta conta = new Conta();
-        conta.saldo = 100;
-        conta.deposita(50);
-        System.out.println(conta.saldo);
 
-        boolean conseguiuRetirar = conta.saca(20);
-        System.out.println(conta.saldo);
-        System.out.println(conseguiuRetirar);
+        Conta contaPaulo = new Conta();
+        Conta contaMarcela = new Conta();
+
+        contaMarcela.saldo = 1000;
+        contaPaulo.saldo = 100;
+
+        //testa deposita
+        contaPaulo.deposita(50);
+        System.out.println(contaPaulo.saldo);
+
+        //testa seca
+        contaPaulo.saca(20);
+        System.out.println(contaPaulo.saldo);
+
+        //verifica retorno boolean
+        boolean verifica = contaPaulo.saca(20);
+        System.out.println(contaPaulo.saldo);
+        System.out.println(verifica);
+
+        contaMarcela.transfere(200, contaPaulo);
+        System.out.println(contaPaulo.saldo);
+        System.out.println(contaMarcela.saldo);
+
+        boolean verifica1 = contaMarcela.transfere(900, contaPaulo);
+        System.out.println(contaMarcela.saldo);
+        System.out.println(verifica1);
+
+
+
     }
 }
